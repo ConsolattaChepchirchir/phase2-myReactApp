8d4317e93a7c9ed050d5a97fa3d00fc9d3638002
ConsolattaChepchirchir <consochep97@gmail.com>
2023-04-17 19:28:10 +0300

    complete task

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import fruits from './fruits.jpg'
+import { buildTimeValue } from '@testing-library/user-event/dist/utils'
+import { type } from '@testing-library/user-event/dist/type'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+function Test() {
+    return(
+        <div>
+        <h1 className ='Text'>Fruits Image</h1>   
+        <img src={fruits} className='fruits'/>
+        <p>The Image above contains different fruits</p>
 
-export default App;
+        <div>
+            <button onClick={() => alert('You clicked this button')}>Click Here</button>
+        </div>
+        </div>
+    )
+   
+}                                                                                                                                                                           
+
+export default Test;
